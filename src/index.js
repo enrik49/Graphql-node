b393b7accab25cd0c0629ca49fca2e36c8c25036
Enric <enrikk49@gmail.com>
2020-10-22 19:02:11 +0200

    Create Prisma database and functions for consult all links and create new link

--- src/index.js
+++ src/index.js
@@ -1,56 +1,49 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { PrismaClient } = require("@prisma/client");
 
-//dummy data
-let links = [{
-    id: 'link-0',
-    url: 'www.aaaa.com',
-    description: 'Fullstack tutorial for GraphQL'
-  },
-  {
-    id: 'link-1',
-    url: 'www.aaaa.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-let idCount = links.length
-
-
+const prisma = new PrismaClient()
 
 const resolvers = {
     Query: {
         info:() => `This is the API`,
         
-        feed: () => links,
-        
-        link: (parent, args) => {
-            return links.find(element => element.id == args.id)
+        feed: async(parent, args, context) => {
+            return context.prisma.link.findMany()
         },
+        
+        /*link: (parent, args) => {
+            return links.find(element => element.id == args.id)
+        },*/
     },
 
     Mutation:{
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                url: args.url,
-                description: args.description
-            }
-            links.push(link)
-            return link
+        post: (parent, args, context) => {
+            const newLink = context.prisma.link.create({
+                data:{
+                    url: args.url,
+                    description: args.description
+                },
+            })
+            return newLink
         },
 
-        updateLink: (parent, args) => {
+        /*updateLink: (parent, args) => {
             const index = links.findIndex(element => element.id == args.id)
             if (args.description) 
                 links[index].description = args.description
             if (args.url) 
                 links[index].url = args.url
             return links.find(element => element.id == args.id)
-        },
+        },*/
     }
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: {
+        prisma,
+    }
 });
 
 server.start(() => console.log('Server is running on port 4000'))
